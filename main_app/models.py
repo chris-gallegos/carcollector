653c8c7113e79d74a41a6ebb2cbe973c10941c27
chris-gallegos <chrisgallegos101@gmail.com>
2022-10-24 16:15:53 -0700

    added details page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    year = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,18 @@
 from django.shortcuts import render
+from .models import Car
 
-from django.http import HttpResponse
 # Create your views here.
 
-class Car:
-    def __init__(self, brand, model, description, year):
-        self.brand = brand
-        self.model = model
-        self.description = description 
-        self.year = year
-
-cars = {
-    Car('Ford', 'Mustang', 'Fast and powerful', 2023),
-    Car('Chevy', 'Camaro', 'discontinued', 2022),
-    Car('Tesla', 'Model 3', 'Electric', 2019),
-    Car('BMW', 'i8', 'Electric', 2018)
-}
-
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render (request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def cars_index(request):
+    cars = Car.objects.all()
     return render(request, 'cars/index.html', { 'cars': cars})
+
+def cars_detail(request, car_id):
+  car = Car.objects.get(id=car_id)
+  return render(request, 'cars/detail.html', { 'car': car })
