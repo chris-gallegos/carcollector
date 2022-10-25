a8b91767fbbc1e7619586943b81391566309b065
chris-gallegos <chrisgallegos101@gmail.com>
2022-10-24 17:11:09 -0700

    added edit and delete functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,3 +9,11 @@ class Car(models.Model):
     model = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     year = models.IntegerField()
+
+
+    def __str__(self):
+        return self.brand
+    
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'car_id': self.id})
